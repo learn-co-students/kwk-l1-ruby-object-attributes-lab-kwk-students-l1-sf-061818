b0186685e43c88ceb669f91073bbc4d5d9565f64
Natalie Sandoval <nsandoval@ma.org>
2018-06-21 22:58:10 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,21 @@
 # person.rb
+class Person
+  def name=(this_name)
+    @name = this_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(this_job)
+    @job = this_job
+  end
+  
+  def job
+    @job
+  end
+end
+
+beyonce = Person.new
+beyonce.name = "Beyonce"
