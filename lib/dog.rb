19e2ec1235fdccbb2028e85919662f7de33d91dd
michellechowdc <michellechowdc@gmail.com>
2018-06-22 05:40:35 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,21 @@
 # dog.rb
+class Dog 
+  def name=(dog_name)
+    @name=dog_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def breed=(dog_breed)
+    @breed=dog_breed
+  end
+  
+  def breed
+    @breed
+  end
+end
+
+# snoopy = Dog.new
+# snoopy.breed = "Beagle"
